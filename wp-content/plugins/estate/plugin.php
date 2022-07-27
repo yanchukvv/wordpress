033fcb64c73bd63172247277b4f301dfe5024789
vladInconver <vlad@Inconver.com>
2022-07-27 18:44:56 +0300

    Подготовка к работе. Установка и очистка wp; установка необходимых плагинов

---
+++ wp-content/plugins/estate/plugin.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: Города и строения
+ * Plugin URI: studio-av.com
+ * Description: Плагин для тестового задания. Добавляет города и строения
+ * Version: 1.0.0
+ * Author: Vladimir Yanchuk
+ * Author URI: https://studio-av.com/
+ * License: GPL-2.0+
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ *
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
